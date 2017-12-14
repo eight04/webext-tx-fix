f01c4f1cbd90d5f6b40d83bd774ba9ce7ab2e0f1
eight <eight04@gmail.com>
2017-12-14 09:08:31 +0800

    Add: strip untranslated string, add test

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const path = require("path");
 const fs = require("fs");
 
+function fixMessages(messages, translate) {
+	let isEdited = false;
+	for (const key of Object.keys(translate)) {
+		if (translate[key].message === messages[key].message) {
+			// strip untranslated messages
+			delete translate[key];
+		} else if (!translate[key].placeholders && messages[key].placeholders) {
+			// re-add placeholders
+			translate[key].placeholders = messages[key].placeholders;
+		} else {
+			continue;
+		}
+		isEdited = true;
+	}
+	return isEdited;
+}
+
 function init({
 	args: {
 		"--source": source = "."
@@ -10,29 +27,15 @@ function init({
 		source = path.resolve(source);
 	}
 	const defaultLocale = require(`${source}/manifest.json`).default_locale;
-	const messages = [...Object.entries(require(`${source}/_locales/${defaultLocale}/messages.json`))]
-		.filter(([, message]) => message.placeholders);
-	if (!messages.length) {
-		console.log("No placeholders");
-		return;
-	}
+	const messages = require(`${source}/_locales/${defaultLocale}/messages.json`);
 	fs.readdirSync(`${source}/_locales`)
 		.filter(l => l != defaultLocale)
 		.forEach(locale => {
 			const filename = `${source}/_locales/${locale}/messages.json`;
-			const translate = require(filename);
-			let isEdited = false;
-			for (const [key, {placeholders}] of messages) {
-				if (translate[key]) {
-					translate[key].placeholders = placeholders;
-					isEdited = true;
-				}
-			}
-			if (isEdited) {
+			const translate = JSON.parse(fs.readFileSync(filename, "utf8"));
+			if (fixMessages(messages, translate)) {
 				fs.writeFileSync(filename, JSON.stringify(translate, null, "\t"), "utf8");
 			}
 		});
 }
-module.exports = {
-	init
-};
+module.exports = {init, fixMessages};
